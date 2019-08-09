869cb4df11150535fceaf33e01b980df304d2572
Luis Chamberlain <mcgrof@kernel.org>
2019-08-09 15:27:13 -0700

    terraform: bump azurerm provider from v1.24.0 to v1.24.0
    To support Terraform 0.12.6, the latest, we need to also upgrade
    the azurerm provider requirements. This fixes this complaint:
    
    Provider "azurerm" v1.24.0 is not compatible with Terraform 0.12.6.
    
    Signed-off-by: Luis Chamberlain <mcgrof@kernel.org>

--- terraform/azure/provider.tf
+++ terraform/azure/provider.tf
@@ -3,8 +3,8 @@
 # described here.
 
 provider "azurerm" {
-  # any non-beta version >= 1.24.0 and < 1.25.0, e.g. 1.24.1
-  version = "~>1.24.0"
+  # any non-beta version >= 1.27.0 and < 1.26.0, e.g. 1.27.1
+  version = "~>1.27.0"
 
   subscription_id             = "${var.subscription_id}"
   client_id                   = "${var.application_id}"
