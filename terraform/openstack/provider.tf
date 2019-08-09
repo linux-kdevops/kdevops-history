c6c649b142ec118dd83d7d8b5db6fa889e243af1
Luis Chamberlain <mcgrof@kernel.org>
2019-08-09 15:38:53 -0700

    openstack: bump openstack provider from v1.11.0 to 1.19.0
    After the terraform upgrade to 0.12.6 terraform now complains about
    needing to upgrade the openstack provider. So bump the provider
    requirements.
    
    Signed-off-by: Luis Chamberlain <mcgrof@kernel.org>

--- terraform/openstack/provider.tf
+++ terraform/openstack/provider.tf
@@ -1,8 +1,8 @@
 # Phones home to Hashicorp to get the required Hashicorp plugins.
 
 provider "openstack" {
-  # any non-beta version >= 1.11.0 and < 1.12.0, e.g. 1.11.1
-  version = "~>1.11.0"
+  # any non-beta version >= 1.19.0 and < 1.20.0, e.g. 1.19.1
+  version = "~>1.19.0"
   # We prefer this method as it means you also get to use standard openstack
   # utilities that also use and share the same configuration files.
   # First clouds-public.yaml is read, then clouds.yaml and last secure.yaml.
