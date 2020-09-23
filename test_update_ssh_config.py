78e525c0238874885737611d7ff3bc5914ed65e6
Luis Chamberlain <mcgrof@kernel.org>
2020-09-23 16:39:06 -0600

    tests: fix flake8 complaints
    Signed-off-by: Luis Chamberlain <mcgrof@kernel.org>

--- test_update_ssh_config.py
+++ test_update_ssh_config.py
@@ -13,6 +13,7 @@ from update_ssh_config import parse_args, run_args
 Unit tests for update_ssh_config.py
 """
 
+
 def get_test_files(function_name):
     test_names = []
     target_sshconfig = 'tests/' + re.sub('^test_', '', function_name)
@@ -27,11 +28,11 @@ def get_test_files(function_name):
     test_names.append(target_sshconfig_bk)
     return test_names
 
+
 class TestUpdateSshConfig(unittest.TestCase):
     def test_0001_remove_hosts_top(self):
         this_function_name = inspect.stack()[0][3]
         tests_names = get_test_files(this_function_name)
-        target_sshconfig = tests_names[0]
         target_sshconfig_orig = tests_names[1]
         target_sshconfig_copy = tests_names[2]
         target_sshconfig_res = tests_names[3]
@@ -46,13 +47,13 @@ class TestUpdateSshConfig(unittest.TestCase):
                            'kdevops,kdevops-dev'])
         run_args(args)
         self.assertTrue(cmp(target_sshconfig_bk,
-                            target_sshconfig_orig, shallow = False))
+                            target_sshconfig_orig, shallow=False))
         self.assertTrue(cmp(target_sshconfig_copy,
-                            target_sshconfig_res, shallow = False))
+                            target_sshconfig_res, shallow=False))
+
     def test_0002_remove_hosts_middle(self):
         this_function_name = inspect.stack()[0][3]
         tests_names = get_test_files(this_function_name)
-        target_sshconfig = tests_names[0]
         target_sshconfig_orig = tests_names[1]
         target_sshconfig_copy = tests_names[2]
         target_sshconfig_res = tests_names[3]
@@ -67,13 +68,13 @@ class TestUpdateSshConfig(unittest.TestCase):
                            'kdevops,kdevops-dev'])
         run_args(args)
         self.assertTrue(cmp(target_sshconfig_bk,
-                            target_sshconfig_orig, shallow = False))
+                            target_sshconfig_orig, shallow=False))
         self.assertTrue(cmp(target_sshconfig_copy,
-                            target_sshconfig_res, shallow = False))
+                            target_sshconfig_res, shallow=False))
+
     def test_0003_remove_hosts_bottom(self):
         this_function_name = inspect.stack()[0][3]
         tests_names = get_test_files(this_function_name)
-        target_sshconfig = tests_names[0]
         target_sshconfig_orig = tests_names[1]
         target_sshconfig_copy = tests_names[2]
         target_sshconfig_res = tests_names[3]
@@ -88,13 +89,13 @@ class TestUpdateSshConfig(unittest.TestCase):
                            'kdevops,kdevops-dev'])
         run_args(args)
         self.assertTrue(cmp(target_sshconfig_bk,
-                            target_sshconfig_orig, shallow = False))
+                            target_sshconfig_orig, shallow=False))
         self.assertTrue(cmp(target_sshconfig_copy,
-                            target_sshconfig_res, shallow = False))
+                            target_sshconfig_res, shallow=False))
+
     def test_0004_remove_hosts_missing(self):
         this_function_name = inspect.stack()[0][3]
         tests_names = get_test_files(this_function_name)
-        target_sshconfig = tests_names[0]
         target_sshconfig_orig = tests_names[1]
         target_sshconfig_copy = tests_names[2]
         target_sshconfig_res = tests_names[3]
@@ -109,13 +110,13 @@ class TestUpdateSshConfig(unittest.TestCase):
                            'kdevops,kdevops-dev'])
         run_args(args)
         self.assertTrue(cmp(target_sshconfig_bk,
-                            target_sshconfig_orig, shallow = False))
+                            target_sshconfig_orig, shallow=False))
         self.assertTrue(cmp(target_sshconfig_copy,
-                            target_sshconfig_res, shallow = False))
+                            target_sshconfig_res, shallow=False))
+
     def test_0005_remove_hosts_similar(self):
         this_function_name = inspect.stack()[0][3]
         tests_names = get_test_files(this_function_name)
-        target_sshconfig = tests_names[0]
         target_sshconfig_orig = tests_names[1]
         target_sshconfig_copy = tests_names[2]
         target_sshconfig_res = tests_names[3]
@@ -130,14 +131,16 @@ class TestUpdateSshConfig(unittest.TestCase):
                            'kdevops,kdevops-dev'])
         run_args(args)
         self.assertTrue(cmp(target_sshconfig_bk,
-                            target_sshconfig_orig, shallow = False))
+                            target_sshconfig_orig, shallow=False))
         self.assertTrue(cmp(target_sshconfig_copy,
-                            target_sshconfig_res, shallow = False))
+                            target_sshconfig_res, shallow=False))
+
     def tearDown(self):
         files = listdir("tests")
         for testfile in files:
             if testfile.endswith(".copy") or testfile.endswith(".bk"):
                 remove(path.join("tests", testfile))
+
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
