d535b9223da8e46505f11d911388be355a379f8c
Luis Chamberlain <mcgrof@kernel.org>
2021-01-07 16:48:57 -0600

    kdevops: add initial kernel-ci support
    This adds some initial kernel continous integration support using the
    fstests workflow as an example. The idea behind this is after you
    establish your baseline for a test you then want to ensure you don't
    regress. To check for regressions you run a workflow test in a loop
    until a failure is found.
    
    This adds support to allow users to email reports for failures.
    
    To run the fstests kernel-ci loop:
    
      make fstests-baseline-loop
    
    Signed-off-by: Luis Chamberlain <mcgrof@kernel.org>

---
+++ scripts/workflows/fstests/run_kernel_ci.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+# SPDX-License-Identifier: GPL-2.0
+
+source ${TOPDIR}/.config
+source ${TOPDIR}/scripts/lib.sh
+
+FSTYPE="$CONFIG_FSTESTS_FSTYP"
+RCPT="ignore@test.com"
+SSH_TARGET="ignore"
+KERNEL_CI_LOOP="${TOPDIR}/scripts/workflows/fstests/run_loop.sh"
+
+if [[ "$CONFIG_KERNEL_CI_EMAIL_REPORT" == "y" ]]; then
+	RCPT="$CONFIG_KERNEL_CI_EMAIL_RCPT"
+	if [[ "$CONFIG_KERNEL_CI_EMAIL_METHOD_SSH" == "y" ]]; then
+		SSH_TARGET="$CONFIG_KERNEL_CI_EMAIL_SSH_HOST"
+	fi
+fi
+
+SUBJECT_PREFIX="kernel-ci: fstests failure for $FSTYPE on test loop "
+
+/usr/bin/time -f %E -o $KERNEL_CI_LOGTIME_FULL $KERNEL_CI_LOOP
+echo "-------------------------------------------" >> $KERNEL_CI_LOGTIME_FULL
+echo "Full run time of kernel-ci loop:" >> $KERNEL_CI_LOGTIME_FULL
+cat $KERNEL_CI_LOGTIME_FULL >> $KERNEL_CI_FAIL_LOG
+
+if [[ "$CONFIG_KERNEL_CI_EMAIL_REPORT" != "y" ]]; then
+	if [[ -f $KERNEL_CI_FAIL_FILE ]]; then
+		FAIL_LOOP="$(cat $KERNEL_CI_FAIL_FILE)"
+		SUBJECT="$SUBJECT_PREFIX $FAIL_LOOP"
+		echo $SUBJECT
+		echo "Full run time of kernel-ci loop:"
+		cat $KERNEL_CI_LOGTIME_FULL
+		exit 1
+	elif [[ -f $KERNEL_CI_OK_FILE ]]; then
+		LOOP_COUNT=$(cat $KERNEL_CI_OK_FILE)
+		SUBJECT="kernel-ci: fstests $FSTYPE never failed after $LOOP_COUNT test loops"
+		echo $SUBJECT
+		echo "Full run time of kernel-ci loop:"
+		cat $KERNEL_CI_LOGTIME_FULL
+		exit 0
+	fi
+fi
+
+if [[ -f $KERNEL_CI_FAIL_FILE ]]; then
+	FAIL_LOOP="$(cat $KERNEL_CI_FAIL_FILE)"
+	SUBJECT="$SUBJECT_PREFIX $FAIL_LOOP"
+	if [[ "$CONFIG_KERNEL_CI_EMAIL_METHOD_LOCAL" == "y" ]]; then
+		cat $KERNEL_CI_FAIL_LOG | mail -s "$SUBJECT" $RCPT
+	elif [[ "$CONFIG_KERNEL_CI_EMAIL_METHOD_SSH" == "y" ]]; then
+		cat $KERNEL_CI_FAIL_LOG | ssh $SSH_TARGET 'mail -s "'$SUBJECT'"' $RCPT
+	fi
+	echo $SUBJECT
+	exit 1
+elif [[ -f $KERNEL_CI_OK_FILE ]]; then
+	LOOP_COUNT=$(cat $KERNEL_CI_OK_FILE)
+	SUBJECT="kernel-ci: fstests $FSTYPE never failed after $LOOP_COUNT test loops"
+	if [[ "$CONFIG_KERNEL_CI_EMAIL_METHOD_LOCAL" == "y" ]]; then
+		cat $KERNEL_CI_FAIL_LOG | mail -s "$SUBJECT" $RCPT
+	elif [[ "$CONFIG_KERNEL_CI_EMAIL_METHOD_SSH" == "y" ]]; then
+		cat $KERNEL_CI_FAIL_LOG | ssh $SSH_TARGET 'mail -s "'$SUBJECT'"' $RCPT
+	fi
+	echo "$SUBJECT"
+	exit 0
+else
+	echo "Unexpected situation after full kernel-ci loop time:"
+	cat $KERNEL_CI_LOGTIME_FULL
+	exit 1
+fi
---
+++ scripts/workflows/fstests/run_loop.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+# SPDX-License-Identifier: GPL-2.0
+
+source ${TOPDIR}/.config
+source ${TOPDIR}/scripts/lib.sh
+
+FSTYPE="$CONFIG_FSTESTS_FSTYP"
+COUNT=1
+
+run_loop()
+{
+	while true; do
+		echo "== kernel-ci fstests $FSTYPE test loop $COUNT start: $(date)" > $KERNEL_CI_FAIL_LOG
+		echo "/usr/bin/time -f %E make fstests-baseline" >> $KERNEL_CI_FAIL_LOG
+		/usr/bin/time -p -o $KERNEL_CI_LOGTIME make fstests-baseline >> $KERNEL_CI_FAIL_LOG
+		echo "End   $COUNT: $(date)" >> $KERNEL_CI_FAIL_LOG
+		cat $KERNEL_CI_LOGTIME >> $KERNEL_CI_FAIL_LOG
+		echo "git status:" >> $KERNEL_CI_FAIL_LOG
+		git status >> $KERNEL_CI_FAIL_LOG
+		echo "Results:" >> $KERNEL_CI_FAIL_LOG
+		XUNIT_FAIL="no"
+		if [ -f workflows/fstests/results/xunit_results.txt ]; then
+			cat workflows/fstests/results/xunit_results.txt >> $KERNEL_CI_FAIL_LOG
+			grep -q Failures workflows/fstests/results/xunit_results.txt
+			if [[ $? -eq 0 ]]; then
+				XUNIT_FAIL="yes"
+			fi
+		fi
+		DIFF_COUNT=$(git diff | wc -l)
+		if [[ "$DIFF_COUNT" -ne 0 || $XUNIT_FAIL=="yes" ]]; then
+			echo "Test  $COUNT: FAILED!" >> $KERNEL_CI_FAIL_LOG
+			echo "== Test loop count $COUNT" >> $KERNEL_CI_FAIL_LOG
+			echo "$(git describe)" >> $KERNEL_CI_FAIL_LOG
+			git diff >> $KERNEL_CI_FAIL_LOG
+			cat $KERNEL_CI_FAIL_LOG >> $KERNEL_CI_FULL_LOG
+			echo $COUNT > $KERNEL_CI_FAIL_FILE
+			exit 1
+		else
+			echo "Test  $COUNT: OK!" >> $KERNEL_CI_FAIL_LOG
+			echo "----------------------------------------------------------------" >> $KERNEL_CI_FAIL_LOG
+			cat $KERNEL_CI_FAIL_LOG >> $KERNEL_CI_FULL_LOG
+		fi
+		echo $COUNT > $KERNEL_CI_OK_FILE
+		let COUNT=$COUNT+1
+		sleep 1
+	done
+}
+
+rm -f $KERNEL_CI_FAIL_FILE $KERNEL_CI_OK_FILE
+echo "= kernel-ci full log" > $KERNEL_CI_FULL_LOG
+run_loop
