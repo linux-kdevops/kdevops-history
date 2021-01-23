a8e4393605db95dc50e5127ce8de925ba5dc2eea
Luis Chamberlain <mcgrof@kernel.org>
2021-01-22 18:22:02 -0600

    kill_pids: a few fixes
    I noticed the top command which runs the kernel-ci process
    was being killed, so we need to be a bit more careful to skip
    that.
    
    Also, provide a few more fixes so you can run this from outside
    of the ansible task context.
    
    Signed-off-by: Luis Chamberlain <mcgrof@kernel.org>

--- scripts/workflows/fstests/kill_pids.sh
+++ scripts/workflows/fstests/kill_pids.sh
@@ -1,18 +1,23 @@
 # SPDX-License-Identifier: GPL-2.0
+if [[ "${TOPDIR}" == "" ]]; then
+	TOPDIR=$PWD
+fi
 source ${TOPDIR}/.config
 source ${TOPDIR}/scripts/lib.sh
 
 FS=$CONFIG_FSTESTS_FSTYP
 STRING='CONFIG_FSTESTS_FSTYP='
-
 PID_LIST=$(ps -fu $USER | awk '{print $2}')
 
 for i in $PID_LIST; do
+	if [[ ! -d /proc/$i ]]; then
+		continue
+	fi
 	CONFIG_TARGET="/proc/$i/cwd/.config"
 	if [[ ! -f  $CONFIG_TARGET ]]; then
 		continue
 	fi
-	FS_TARGET=$(grep $STRING /proc/$i/cwd/.config | awk -F"=" '{print $2}' | sed -e 's|"||g')
+	FS_TARGET=$(grep $STRING $CONFIG_TARGET | awk -F"=" '{print $2}' | sed -e 's|"||g')
 	if [[ "$FS" != "$FS_TARGET" ]]; then
 		continue
 	fi
@@ -22,23 +27,36 @@ for i in $PID_LIST; do
 		continue
 	fi
 
-	grep -q ansible-playbook /proc/$i/cmdline
+	grep -q "fstests-baseline-loop" /proc/$i/cmdline
 	if [[ $? -eq 0 ]]; then
-		kill -SIGTERM -- -${i}
-		kill -SIGALRM -- -${i}
-		kill -SIGALRM ${i}
 		continue
 	fi
 
 	grep -q make /proc/$i/cmdline
 	if [[ $? -eq 0 ]]; then
-		kill -SIGTERM -- -${i}
-		kill -SIGTERM ${i}
+		kill -SIGTERM -- -${i} 2>/dev/null
+		kill -SIGTERM ${i} 2>/dev/null
+	fi
+
+	grep -q run_loop /proc/$i/cmdline
+	if [[ $? -eq 0 ]]; then
+		kill -SIGTERM -- -${i} 2>/dev/null
+		kill -SIGTERM ${i} 2>/dev/null
+	fi
+
+	grep -q ansible-playbook /proc/$i/cmdline
+	if [[ $? -eq 0 ]]; then
+		cat /proc/$i/cmdline
+		echo
+		kill -SIGTERM -- -${i} 2>/dev/null
+		kill -SIGALRM -- -${i} 2>/dev/null
+		kill -SIGALRM ${i} 2>/dev/null
+		continue
 	fi
 
 	grep -q ssh /proc/$i/cmdline
 	if [[ $? -eq 0 ]]; then
-		kill -SIGTERM -- -${i}
-		kill -SIGTERM ${i}
+		kill -SIGTERM -- -${i} 2>/dev/null
+		kill -SIGTERM ${i} 2>/dev/null
 	fi
 done
