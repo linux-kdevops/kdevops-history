3b2df94f96bde5514d4446db12cf5af452d4b8af
Luis Chamberlain <mcgrof@kernel.org>
2021-02-07 18:07:27 -0600

    fstests: split up finding the files and processing them
    When looking into the odd bug I was seeing on processsing the
    expunge files I initially could not tell what was causing the
    issue. To be even safer, let's split off the os walk from
    post processing of the files.
    
    This also lets us move the processing of the files with one less
    indent and makes the code easier to read.
    
    While at it, add a verbose argument to allow us to inspect things
    step by step.
    
    Signed-off-by: Luis Chamberlain <mcgrof@kernel.org>

--- playbooks/python/workflows/fstests/augment_expunge_list.py
+++ playbooks/python/workflows/fstests/augment_expunge_list.py
@@ -30,16 +30,18 @@ def main():
                         help='directory with results file')
     parser.add_argument('outputdir', metavar='<output directory>', type=str,
                         help='The directory where to generate the expunge lists to')
+    parser.add_argument('--verbose', const=True, default=False, action="store_const",
+                        help='Print more verbose information')
     args = parser.parse_args()
 
     expunge_kernel_dir = ""
 
-    all_files = os.listdir(args.results)
+#    all_files = os.listdir(args.results)
 
+    bad_files = []
     for root, dirs, all_files in os.walk(args.results):
         for fname in all_files:
             f = os.path.join(root, fname)
-            #sys.stdout.write("%s\n" % f)
             if os.path.isdir(f):
                 continue
             if not os.path.isfile(f):
@@ -47,61 +49,73 @@ def main():
             if not f.endswith('.bad'):
                 continue
 
-            # f may be results/oscheck-xfs/4.19.0-4-amd64/xfs/generic/xxx.out.bad
-            # where xxx are digits
-            bad_file_list = f.split("/")
-            bad_file_list_len = len(bad_file_list) - 1
-            bad_file = bad_file_list[bad_file_list_len]
-            test_type = bad_file_list[bad_file_list_len-1]
-            section = bad_file_list[bad_file_list_len-2]
-            kernel = bad_file_list[bad_file_list_len-3]
-            hostname = bad_file_list[bad_file_list_len-4]
+            bad_files.append(f)
+    for f in bad_files:
+        if args.verbose:
+            sys.stdout.write("Processing %s\n" % f)
 
-            bad_file_parts = bad_file.split(".")
-            bad_file_part_len = len(bad_file_parts) - 1
-            bad_file_test_number = bad_file_parts[bad_file_part_len - 2]
-            # This is like for example generic/xxx where xxx are digits
-            test_failure_line = test_type + '/' + bad_file_test_number
+        # f may be results/oscheck-xfs/4.19.0-4-amd64/xfs/generic/xxx.out.bad
+        # where xxx are digits
+        bad_file_list = f.split("/")
+        bad_file_list_len = len(bad_file_list) - 1
+        bad_file =      bad_file_list[bad_file_list_len]
+        test_group =    bad_file_list[bad_file_list_len-1]
+        section =       bad_file_list[bad_file_list_len-2]
+        kernel =        bad_file_list[bad_file_list_len-3]
+        hostname =      bad_file_list[bad_file_list_len-4]
 
-            # now to stuff this into expunge files such as:
-            # path/4.19.17/xfs/unassigned/xfs_nocrc.txt
-            expunge_kernel_dir = args.outputdir + '/' + kernel + '/' + args.filesystem + '/'
-            output_dir = expunge_kernel_dir + 'unassigned/'
-            output_file = output_dir + section + '.txt'
-            shortcut_kernel_dir = None
-            shortcut_dir = None
-            shortcut_file = None
+        if args.verbose:
+            sys.stdout.write("%s\n" % bad_file_list)
+            sys.stdout.write("\tbad_file: %s\n" %bad_file)
+            sys.stdout.write("\ttest_group: %s\n" % test_group)
+            sys.stdout.write("\tsection: %s\n" % section)
+            sys.stdout.write("\thostname: %s\n" % hostname)
 
-            if hostname.startswith("sles"):
-                ksplit = kernel.split(".")
-                shortcut_kernel = ksplit[0] + "." + ksplit[1] + "." + ksplit[2]
-                shortcut_kernel_dir = args.outputdir + '/' + shortcut_kernel + '/' + args.filesystem + '/'
-                shortcut_dir = shortcut_kernel_dir + 'unassigned/'
-                shortcut_file = shortcut_dir + section + '.txt'
+        bad_file_parts = bad_file.split(".")
+        bad_file_part_len = len(bad_file_parts) - 1
+        bad_file_test_number = bad_file_parts[bad_file_part_len - 2]
+        # This is like for example generic/xxx where xxx are digits
+        test_failure_line = test_group + '/' + bad_file_test_number
 
-            if not os.path.isdir(output_dir):
-                if shortcut_dir and os.path.isdir(shortcut_dir):
-                    output_dir = shortcut_dir
-                    output_file = shortcut_file
-                    expunge_kernel_dir = shortcut_kernel_dir
-                else:
-                    os.makedirs(output_dir)
+        # now to stuff this into expunge files such as:
+        # path/4.19.17/xfs/unassigned/xfs_nocrc.txt
+        expunge_kernel_dir = args.outputdir + '/' + kernel + '/' + args.filesystem + '/'
+        output_dir = expunge_kernel_dir + 'unassigned/'
+        output_file = output_dir + section + '.txt'
+        shortcut_kernel_dir = None
+        shortcut_dir = None
+        shortcut_file = None
 
-            if not os.path.isfile(output_file):
-                sys.stdout.write("%s %s new failure found file was empty\n" % (section, test_failure_line))
-                append_line(output_file, test_failure_line)
+        if hostname.startswith("sles"):
+            ksplit = kernel.split(".")
+            shortcut_kernel = ksplit[0] + "." + ksplit[1] + "." + ksplit[2]
+            shortcut_kernel_dir = args.outputdir + '/' + shortcut_kernel + '/' + args.filesystem + '/'
+            shortcut_dir = shortcut_kernel_dir + 'unassigned/'
+            shortcut_file = shortcut_dir + section + '.txt'
+
+        if not os.path.isdir(output_dir):
+            if shortcut_dir and os.path.isdir(shortcut_dir):
+                output_dir = shortcut_dir
+                output_file = shortcut_file
+                expunge_kernel_dir = shortcut_kernel_dir
             else:
-                existing_file = open(output_file, 'r')
-                all_lines = existing_file.readlines()
-                existing_file.close()
-                found = False
-                for line in all_lines:
-                    if test_failure_line in line:
-                        found = True
-                        break
-                if not found:
-                    sys.stdout.write("%s %s new failure found\n" % (section, test_failure_line))
-                    append_line(output_file, test_failure_line)
+                os.makedirs(output_dir)
+
+        if not os.path.isfile(output_file):
+            sys.stdout.write("====%s %s new failure found file was empty\n" % (section, test_failure_line))
+            append_line(output_file, test_failure_line)
+        else:
+            existing_file = open(output_file, 'r')
+            all_lines = existing_file.readlines()
+            existing_file.close()
+            found = False
+            for line in all_lines:
+                if test_failure_line.strip() in line.strip():
+                    found = True
+                    break
+            if not found:
+                sys.stdout.write("%s %s new failure found\n" % (section, test_failure_line))
+                append_line(output_file, test_failure_line)
 
     if expunge_kernel_dir != "":
         sys.stdout.write("Sorting %s ...\n" % (expunge_kernel_dir))
