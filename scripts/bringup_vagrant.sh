e0a82ea04bc744ba144711baf6e3635b2b88d07a
Josef Bacik <josef@toxicpanda.com>
2022-05-17 17:12:31 -0400

    kdevops: use --no-parallel for broken version of vagrant-libvirt
    This bit me on fedora, we need to use --no-parallel until we get an
    updated package of vagrant-libvirt deployed.
    
    Signed-off-by: Josef Bacik <josef@toxicpanda.com>

--- scripts/bringup_vagrant.sh
+++ scripts/bringup_vagrant.sh
@@ -6,5 +6,11 @@ set -e
 source ${TOPDIR}/.config
 source ${TOPDIR}/scripts/lib.sh
 
+# This is just a workaround for fedora since we have an old vagrant-libvirt
+# plugin that doesn't work with parallel
+ARG=
+vagrant plugin list | grep -q 'vagrant-libvirt (0.7.0, system)'
+[ $? -eq 0 ] && ARG='--no-parallel'
+
 cd vagrant
-vagrant up
+vagrant up $ARG
