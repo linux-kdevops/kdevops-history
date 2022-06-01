linux-kdevops/kdevops-history

commit ecabd29de4c99906e2789487b2cf41742192b69c

kdevops: add initial qemu build support and add a CXL enhanced repo
This adds initial build support for qemu so to enable working
with technology which is not available yet on the versions of qemu
carried by distributions yet.

Examples for this is CXL support which does not yet have
support through a qemu package on most distributions, so
the only thing you can do is build qemu from scratch.

This adds the basic initial distribution support for all the
distributions we currently support. This just let's us build
qemu and install it.

The next steps will be to add kconfig logic to take advatage
of some bleeding edge tech which is still under development
such as CXL.

Building qemu from source is obviously useful beyond CXL, this
is just an example tech which is not yet upstream.

Signed-off-by: Luis Chamberlain <mcgrof@kernel.org>

Makefile
@@ -112,6 +112,12 @@ endif
 # make with no arguments.
 LOCALHOST_SETUP_WORK :=
 
+QEMU_BUILD_SETUP_ARGS :=
+ifeq (y,$(CONFIG_QEMU_BUILD))
+include Makefile.build_qemu
+endif # CONFIG_QEMU_BUILD
+ANSIBLE_EXTRA_ARGS += $(QEMU_BUILD_SETUP_ARGS)
+
 POSTFIX_SETUP_ARGS :=
 ifeq (y,$(CONFIG_SETUP_POSTFIX_EMAIL_RELAY))
 include Makefile.postfix

playbooks/roles/build_qemu/tasks/main.yml
@@ -1,0 +1,83 @@
+---
+- name: Import optional extra_args file
+  include_vars: "{{ item }}"
+  ignore_errors: yes
+  with_first_found:
+    - files:
+      - "../extra_vars.yml"
+      - "../extra_vars.yaml"
+      - "../extra_vars.json"
+      skip: true
+  tags: vars
+
+- name: Verify local build qemu installation
+  stat:
+    path: "{{ qemu_bin_path }}"
+  register: qemu_present
+  changed_when: true
+  failed_when: qemu_present.stat.exists and not qemu_present.stat.executable
+  tags: [ 'qemu', 'verify' ]
+  when:
+    - qemu_build|bool
+
+- name: Install build-deps for qemu as per each Linux distribution
+  include_tasks: install-deps/main.yml
+  when:
+    - qemu_build|bool
+    - qemu_force_install_if_present|bool or not qemu_present.stat.exists
+
+- name: Assume we won't build qemu first
+  set_fact:
+    build_qemu_now: False
+
+- name: Annotate when we are building qemu
+  set_fact:
+    build_qemu_now: True
+  when:
+    - qemu_build|bool
+    - qemu_force_install_if_present|bool or (qemu_present.stat is not defined)
+
+- name: git clone qemu
+  environment:
+    GIT_SSL_NO_VERIFY:  true
+  git:
+    repo: "{{ qemu_git }}"
+    dest: "{{ qemu_data }}"
+  when:
+    - build_qemu_now|bool
+
+- name: Create a qemu build directory
+  file:
+    path: "{{ qemu_build_dir }}"
+    state: directory
+  tags: [ 'qemu', 'build-deps' ]
+  when:
+    - build_qemu_now|bool
+
+- name: run configure for qemu
+  command: "../configure"
+  tags: [ 'qemu', 'configure' ]
+  args:
+    chdir: "{{ qemu_build_dir }}"
+  when:
+    - build_qemu_now|bool
+
+- name: Build qemu
+  make:
+    chdir: "{{ qemu_build_dir }}"
+    params:
+      NUM_THREADS: "{{ make_num_jobs }}"
+  tags: [ 'qemu', 'build' ]
+  when:
+    - build_qemu_now|bool
+
+- name: Install qemu
+  become: yes
+  become_flags: 'su - -c'
+  become_method: sudo
+  command: "{{ make }} install"
+  args:
+    chdir: "{{ qemu_build_dir }}"
+  tags: [ 'qemu', 'install' ]
+  when:
+    - build_qemu_now|bool

vagrant/Kconfig
@@ -20,6 +20,17 @@ endchoice
 
 if VAGRANT_LIBVIRT
 
+config QEMU_BUILD
+	bool "Should we build qemu for you?"
+	help
+	  You only want to enable this option if your distribution package
+	  of qemu does not have support for the features you need. For
+	  example this may be useful if you are a qemu developer or are
+	  relying on technology is still under development or if you have
+	  a custom qemu git URL.
+
+if !QEMU_BUILD
+
 config QEMU_USE_DEVELOPMENT_VERSION
 	bool "Should we look for a development version of qemu?"
 	help
@@ -28,6 +39,54 @@ config QEMU_USE_DEVELOPMENT_VERSION
 	  you are going to be working on.
 
 	  Say yes here if you are compiling your own version of qemu.
+
+endif # !QEMU_BUILD
+
+if QEMU_BUILD
+
+choice
+	prompt "Qemu git URL to use"
+	default QEMU_BUILD_UPSTREAM
+
+config QEMU_BUILD_UPSTREAM
+	bool "https://github.com/qemu/qemu.git"
+	help
+	  Select this option if you want to use the upstream qemu git repo.
+
+config QEMU_BUILD_JIC23
+	bool "https://gitlab.com/jic23/qemu.git"
+	help
+	  Select this option if you want to use Cameron's qemu git repo.
+	  This has a few CXL bells and whistles which are not yet upstream.
+
+config QEMU_BUILD_MANUAL
+	bool "Custom qemu git URL"
+	help
+	  Select this option if you want to specify your own git URL.
+
+endchoice
+
+config QEMU_BUILD_GIT
+	string "Git tree for qemu to clone on localhost"
+	default "https://github.com/qemu/qemu.git" if QEMU_BUILD_UPSTREAM || QEMU_BUILD_MANUAL
+	default "https://gitlab.com/jic23/qemu.git" if QEMU_BUILD_JIC23
+	help
+	  This is the git URL to use to clone and then build qemu for you on
+	  your localhost.
+
+config QEMU_BUILD_GIT_DATA_PATH
+	string "The destination directory where to clone the qemu git tree"
+	default "{{data_path}}/qemu"
+	help
+	  This is the target location of where to clone the above git tree.
+	  Note that {{data_path}} corresponds to the location set by the
+	  configuration option CONFIG_WORKFLOW_DATA_PATH.
+
+config QEMU_USE_DEVELOPMENT_VERSION
+	bool
+	default y
+
+endif # QEMU_BUILD
 
 config QEMU_BIN_PATH_LIBVIRT
 	string "Qemu binary path to use"

playbooks/roles/build_qemu/defaults/main.yml
@@ -1,0 +1,12 @@
+# SPDX-License-Identifier copyleft-next-0.3.1
+---
+
+qemu_build: False
+
+# Forces to build and install even if the file
+# /usr/local/bin/qemu-system-x86_64 is already present
+qemu_force_install_if_present: False
+qemu_bin_path: "/usr/local/bin/qemu-system-x86_64"
+qemu_data: "{{ data_path }}/qemu"
+qemu_git: "https://github.com/qemu/qemu.git"
+qemu_build_dir: "{{ qemu_data }}/build"

playbooks/roles/build_qemu/tasks/install-deps/debian/main.yml
@@ -1,0 +1,74 @@
+---
+- name: Update apt cache
+  become: yes
+  become_method: sudo
+  apt:
+    update_cache: yes
+  tags: [ 'qemu', 'update-cache' ]
+
+- name: Install qemu build dependencies
+  become: yes
+  become_method: sudo
+  apt:
+    name:
+      - debhelper-compat
+      - glusterfs-common
+      - gnutls-dev
+      - libaio-dev
+      - libasound2-dev
+      - libbpf-dev
+      - libbrlapi-dev
+      - libcacard-dev
+      - libcap-ng-dev
+      - libcapstone-dev
+      - libcurl4-gnutls-dev
+      - libdrm-dev
+      - libepoxy-dev
+      - libfdt-dev
+      - libfuse3-dev
+      - libgbm-dev
+      - libglusterfs-dev
+      - libgtk-3-dev
+      - libibumad-dev
+      - libibverbs-dev
+      - libiscsi-dev
+      - libjack-dev
+      - libjpeg-dev
+      - libncurses-dev
+      - libnfs-dev
+      - libnuma-dev
+      - libpixman-1-dev
+      - libpmem-dev
+      - libpng-dev
+      - libpulse-dev
+      - librbd-dev
+      - librdmacm-dev
+      - libsasl2-dev
+      - libsdl2-dev
+      - libseccomp-dev
+      - libslirp-dev
+      - libspice-server-dev
+      - libssh-dev
+      - libudev-dev
+      - liburing-dev
+      - libusb-1.0-0-dev
+      - libusbredirparser-dev
+      - libvdeplug-dev
+      - libvirglrenderer-dev
+      - libvte-2.91-dev
+      - libxen-dev
+      - libzstd-dev
+      - meson
+      - nettle-dev
+      - ninja-build
+      - python3
+      - python3-sphinx
+      - python3-sphinx-rtd-theme
+      - texinfo
+      - uuid-dev
+      - zlib1g-dev
+      - device-tree-compiler
+      - fcode-utils
+      - xsltproc
+    state: present
+  tags: [ 'qemu', 'build-deps' ]

playbooks/roles/build_qemu/tasks/install-deps/fedora/main.yml
@@ -1,0 +1,274 @@
+---
+- name: Verify local build qemu installation
+  command: "which qemu-system-x86_64"
+  register: qemu_present
+  changed_when: qemu_present.rc == 1
+  failed_when: qemu_present.rc != 0 and qemu_present.rc != 1
+  tags: [ 'qemu', 'verify' ]
+
+- name: Install qemu build dependencies
+  become: yes
+  become_method: sudo
+  dnf:
+    name:
+      - ImageMagick
+      - ImageMagick-libs
+      - LibRaw
+      - SDL2
+      - SDL2-devel
+      - SDL2_image
+      - SDL2_image-devel
+      - alsa-lib-devel
+      - at-spi2-atk-devel
+      - at-spi2-core-devel
+      - atk-devel
+      - binutils
+      - binutils-gold
+      - bison
+      - bluez-libs
+      - boost-filesystem
+      - boost-system
+      - boost-thread
+      - brlapi
+      - brlapi-devel
+      - brltty
+      - brotli
+      - brotli-devel
+      - bzip2-devel
+      - cairo-devel
+      - cairo-gobject-devel
+      - capstone
+      - capstone-devel
+      - cmake-filesystem
+      - cpp
+      - cyrus-sasl
+      - cyrus-sasl-devel
+      - daxctl-devel
+      - daxctl-libs
+      - dbus-devel
+      - device-mapper-multipath
+      - device-mapper-multipath-devel
+      - device-mapper-multipath-libs
+      - dyninst
+      - elfutils-debuginfod-client-devel
+      - elfutils-devel
+      - elfutils-libelf-devel
+      - emacs-filesystem
+      - flex
+      - fontawesome-fonts
+      - fontconfig-devel
+      - freetype-devel
+      - fribidi-devel
+      - fuse-devel
+      - fuse3-devel
+      - gc
+      - gcc
+      - gdk-pixbuf2-devel
+      - glib2-devel
+      - glib2-static
+      - glibc-devel
+      - glibc-headers
+      - glibc-static
+      - glibmm24
+      - glusterfs
+      - gmp-c++
+      - gmp-devel
+      - gnutls-c++
+      - gnutls-dane
+      - gnutls-devel
+      - google-roboto-slab-fonts
+      - graphite2-devel
+      - gtk3-devel
+      - guile22
+      - harfbuzz-devel
+      - harfbuzz-icu
+      - imath
+      - jack-audio-connection-kit
+      - jack-audio-connection-kit-devel
+      - jasper-libs
+      - kernel-devel
+      - kernel-headers
+      - lato-fonts
+      - libX11-devel
+      - libXau-devel
+      - libXcomposite-devel
+      - libXcursor-devel
+      - libXdamage-devel
+      - libXext-devel
+      - libXfixes-devel
+      - libXft-devel
+      - libXi-devel
+      - libXinerama-devel
+      - libXrandr-devel
+      - libXrender-devel
+      - libXtst-devel
+      - libacl-devel
+      - libaio
+      - libaio-devel
+      - libattr-devel
+      - libblkid-devel
+      - libbpf
+      - libbpf-devel
+      - libcacard
+      - libcacard-devel
+      - libcap-ng-devel
+      - libcloudproviders-devel
+      - libconfig
+      - libcurl-devel
+      - libdatrie-devel
+      - libdecor
+      - libdrm-devel
+      - libepoxy-devel
+      - libfdt
+      - libfdt-devel
+      - libffado
+      - libffi-devel
+      - libgfapi-devel
+      - libgfapi0
+      - libgfrpc-devel
+      - libgfrpc0
+      - libgfxdr-devel
+      - libgfxdr0
+      - libglusterfs-devel
+      - libglusterfs0
+      - libglvnd-core-devel
+      - libglvnd-devel
+      - libglvnd-gles
+      - libglvnd-opengl
+      - libicu-devel
+      - libidn2-devel
+      - libiec61883
+      - libiscsi
+      - libiscsi-devel
+      - libjpeg-turbo-devel
+      - liblqr-1
+      - libmount-devel
+      - libmpc
+      - libnfs
+      - libnfs-devel
+      - libpciaccess-devel
+      - libpkgconf
+      - libpng-devel
+      - libraqm
+      - libraw1394
+      - librdmacm
+      - libsamplerate
+      - libseccomp-devel
+      - libselinux-devel
+      - libsepol-devel
+      - libsigc++20
+      - libslirp
+      - libslirp-devel
+      - libssh-devel
+      - libtasn1-devel
+      - libtasn1-tools
+      - libthai-devel
+      - libtiff-devel
+      - libusb1-devel
+      - libuuid-devel
+      - libwmf-lite
+      - libxcb-devel
+      - libxcrypt-devel
+      - libxcrypt-static
+      - libxkbcommon-devel
+      - libxml++
+      - libxml2-devel
+      - libzstd-devel
+      - lzo-devel
+      - lzo-minilzo
+      - m4
+      - make
+      - man-pages
+      - mesa-libEGL-devel
+      - mesa-libgbm-devel
+      - meson
+      - ncurses-c++-libs
+      - ncurses-devel
+      - nettle-devel
+      - ninja-build
+      - nspr-devel
+      - nss-devel
+      - nss-softokn-devel
+      - nss-softokn-freebl-devel
+      - nss-util-devel
+      - openexr-libs
+      - openssl-devel
+      - p11-kit-devel
+      - pam-devel
+      - pango-devel
+      - pcre-cpp
+      - pcre-devel
+      - pcre-static
+      - pcre-utf16
+      - pcre-utf32
+      - pcre2-devel
+      - pcre2-utf16
+      - pcre2-utf32
+      - pcsc-lite-devel
+      - pcsc-lite-libs
+      - perl-Benchmark
+      - perl-Test-Harness
+      - perl-Text-Unidecode
+      - perl-Unicode-EastAsianWidth
+      - perl-Unicode-Normalize
+      - perl-libintl-perl
+      - pixman-devel
+      - pkgconf-1.8.0
+      - pkgconf-m4
+      - pkgconf-pkg-config
+      - polkit-libs
+      - pulseaudio-libs-devel
+      - pulseaudio-libs-glib2-15.0
+      - python3-devel
+      - python3-docutils
+      - python3-imagesize
+      - python3-packaging
+      - python3-pip
+      - python3-pygments
+      - python3-pyparsing
+      - python3-snowballstemmer
+      - python3-sphinx
+      - python3-sphinx-theme-alabaster
+      - python3-sphinx_rtd_theme
+      - python3-sphinxcontrib-applehelp
+      - python3-sphinxcontrib-devhelp
+      - python3-sphinxcontrib-htmlhelp
+      - python3-sphinxcontrib-jsmath
+      - python3-sphinxcontrib-qthelp
+      - python3-sphinxcontrib-serializinghtml
+      - snappy
+      - snappy-devel
+      - spice-protocol
+      - spice-server
+      - spice-server-devel
+      - sysprof-capture-devel
+      - systemd-devel
+      - systemtap
+      - systemtap-client
+      - systemtap-devel
+      - systemtap-runtime
+      - systemtap-sdt-devel
+      - tbb-2020.3
+      - texinfo
+      - unzip
+      - usbredir
+      - usbredir-devel
+      - userspace-rcu
+      - valgrind
+      - valgrind
+      - virglrenderer
+      - virglrenderer
+      - vte-profile
+      - vte291
+      - vte291
+      - wayland-devel
+      - xen-devel
+      - xen-libs
+      - xen-licenses
+      - xorg-x11-proto-devel
+      - xz-devel
+      - yajl
+      - zip
+      - zlib-devel
+      - zlib-static
+    state: present

playbooks/roles/build_qemu/tasks/install-deps/main.yml
@@ -1,0 +1,21 @@
+---
+- name: Import optional distribution specific variables
+  include_vars: "{{ item }}"
+  ignore_errors: yes
+  with_first_found:
+    - files:
+      - "{{ ansible_facts['os_family']|lower }}.yml"
+      skip: true
+  tags: vars
+
+- name: distribution specific setup
+  import_tasks: debian/main.yml
+  when: ansible_facts['os_family']|lower == 'debian'
+- import_tasks: suse/main.yml
+  when: ansible_facts['os_family']|lower == 'suse'
+- import_tasks: redhat/main.yml
+  when:
+    - ansible_facts['os_family']|lower == 'redhat'
+    - ansible_facts['distribution']|lower != "fedora"
+- import_tasks: fedora/main.yml
+  when: ansible_facts['distribution']|lower == "fedora"

playbooks/roles/build_qemu/tasks/install-deps/redhat/main.yml
@@ -1,0 +1,65 @@
+---
+- name: Install qemu build dependencies
+  become: yes
+  become_method: sudo
+  dnf:
+    name:
+      - glusterfs-common
+      - gnutls-dev
+      - libaio-dev
+      - libasound2-dev
+      - libbpf-dev
+      - libbrlapi-dev
+      - libcacard-dev
+      - libcap-ng-dev
+      - libcapstone-dev
+      - libcurl4-gnutls-dev
+      - libdrm-dev
+      - libepoxy-dev
+      - libfdt-dev
+      - libfuse3-dev
+      - libgbm-dev
+      - libglusterfs-dev
+      - libgtk-3-dev
+      - libibumad-dev
+      - libibverbs-dev
+      - libiscsi-dev
+      - libjack-dev
+      - libjpeg-dev
+      - libncurses-dev
+      - libnfs-dev
+      - libnuma-dev
+      - libpixman-1-dev
+      - libpmem-dev
+      - libpng-dev
+      - libpulse-dev
+      - librbd-dev
+      - librdmacm-dev
+      - libsasl2-dev
+      - libsdl2-dev
+      - libseccomp-dev
+      - libslirp-dev
+      - libspice-server-dev
+      - libssh-dev
+      - libudev-dev
+      - liburing-dev
+      - libusb-1.0-0-dev
+      - libusbredirparser-dev
+      - libvdeplug-dev
+      - libvirglrenderer-dev
+      - libvte-2.91-dev
+      - libxen-dev
+      - libzstd-dev
+      - meson
+      - nettle-dev
+      - ninja-build
+      - python3
+      - python3-sphinx
+      - python3-sphinx-rtd-theme
+      - texinfo
+      - uuid-dev
+      - zlib1g-dev
+      - device-tree-compiler
+      - fcode-utils
+      - xsltproc
+    state: present

commit 4ea3e9978a5391ee5c11b92813c0756d43c12e0b

build_qemu: add an optional directory for localhost builds
qemu is the first component which we optionaly allow building
locally on kdevops onto the host. The assumption before was
that we were only building things on the guest, and used
{{ data_path }} for that. But since that is for the target
nodes / guests it is incorrect to use that. So we need to
define a new diretory for local builds. This directory
is not needed to be exposed as an option if you are not
building qemu, and so let's not confuse users with this
and only expose it *iff* the user is building qemu.

We add the CONFIG_NEEDS_LOCAL_DEVELOPMENT_PATH bool to let
other future kconfig entries which may need this variable
as well. They would just need to select it as we do now with
CONFIG_QEMU_BUILD.

Fixes: ecabd29de4c9 ("kdevops: add initial qemu build support and add a CXL enhanced repo")
Signed-off-by: Luis Chamberlain <mcgrof@kernel.org>

Kconfig
@@ -9,6 +9,10 @@ config HAVE_KDEVOPS_CUSTOM_DEFAULTS
 	bool
 	default n
 
+config NEEDS_LOCAL_DEVELOPMENT_PATH
+	bool
+	default n
+
 config KDEVOPS_FIRST_RUN
 	bool "Is this your first time running kdevops on this system?"
 	default n
@@ -215,6 +219,13 @@ config SETUP_POSTFIX_EMAIL_RELAY_HOST
 	  created.
 
 endif
+
+config LOCAL_DEVELOPMENT_PATH
+	string "The path to place source code on localhost if we building things"
+	default $(shell, echo $HOME/devel/)
+	depends on NEEDS_LOCAL_DEVELOPMENT_PATH
+	help
+	  The path where kdevops should put code we need to compile.
 
 menu "Bring up methods"
 source "kconfigs/Kconfig.bringup"

Makefile
@@ -56,6 +56,13 @@ export KDEVOPS_HOSTS := $(KDEVOPS_HOSTFILE)
 # configuration data for ansible roles through kconfig.
 ANSIBLE_EXTRA_ARGS :=
 
+LOCAL_DEVELOPMENT_ARGS	:=
+ifeq (y,$(CONFIG_NEEDS_LOCAL_DEVELOPMENT_PATH))
+include local.Makefile
+endif # CONFIG_NEEDS_LOCAL_DEVELOPMENT_PATH
+
+ANSIBLE_EXTRA_ARGS += $(LOCAL_DEVELOPMENT_ARGS)
+
 # These should be set as non-empty if you want any generic bring up
 # targets to come up. We support 2 bring up methods:
 #

local.Makefile
@@ -1,0 +1,4 @@
+# SPDX-License-Identifier: copyleft-next-0.3.1
+
+LOCAL_DEVELOPMENT_PATH:=$(subst ",,$(CONFIG_LOCAL_DEVELOPMENT_PATH))
+LOCAL_DEVELOPMENT_ARGS += local_dev_path=$(LOCAL_DEVELOPMENT_PATH)

playbooks/roles/build_qemu/tasks/main.yml
@@ -39,6 +39,14 @@
     - qemu_force_install_if_present|bool or (qemu_present.stat is not defined)
   tags: vars
 
+- name: Ensure that {{ local_dev_path }} exists
+  file:
+    path: "{{ local_dev_path }}"
+    state: directory
+  tags: [ 'qemu', 'build-deps' ]
+  when:
+    - build_qemu_now|bool
+
 - name: git clone qemu
   environment:
     GIT_SSL_NO_VERIFY:  true

vagrant/Kconfig
@@ -22,6 +22,7 @@ if VAGRANT_LIBVIRT
 
 config QEMU_BUILD
 	bool "Should we build qemu for you?"
+	select NEEDS_LOCAL_DEVELOPMENT_PATH
 	help
 	  You only want to enable this option if your distribution package
 	  of qemu does not have support for the features you need. For
@@ -76,11 +77,11 @@ config QEMU_BUILD_GIT
 
 config QEMU_BUILD_GIT_DATA_PATH
 	string "The destination directory where to clone the qemu git tree"
-	default "{{data_path}}/qemu"
+	default "{{local_dev_path}}/qemu"
 	help
 	  This is the target location of where to clone the above git tree.
-	  Note that {{data_path}} corresponds to the location set by the
-	  configuration option CONFIG_WORKFLOW_DATA_PATH.
+	  Note that {{local_dev_path}} corresponds to the location set by the
+	  configuration option CONFIG_NEEDS_LOCAL_DEVELOPMENT_PATH.
 
 config QEMU_USE_DEVELOPMENT_VERSION
 	bool