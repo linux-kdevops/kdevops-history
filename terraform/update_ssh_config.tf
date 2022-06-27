901e1a7236024f934baa26361e4d3193609a21ae
Luis Chamberlain <mcgrof@kernel.org>
2022-06-27 14:47:15 -0700

    terraform: migreate to the linux-kdevops/add-host-ssh-config module
    Since we are sharing code now under the linux-kdevops organization
    make the module come from linux-kdevops on the registry. This updated
    module also has a small fix for when you .ssh/config file is empty.
    
    Signed-off-by: Luis Chamberlain <mcgrof@kernel.org>

--- terraform/update_ssh_config.tf
+++ terraform/update_ssh_config.tf
@@ -1,6 +1,6 @@
 module "ssh_config_update_host_entries" {
-  source  = "mcgrof/add-host-ssh-config/kdevops"
-  version = "2.3.2"
+  source  = "linux-kdevops/add-host-ssh-config/kdevops"
+  version = "3.0.0"
 
   ssh_config = var.ssh_config
   update_ssh_config_enable = local.limit_count > 0 ? "true" : ""
