11c5688d4041b024a7bbc271427261223acb757e
Luis Chamberlain <mcgrof@kernel.org>
2022-11-19 00:17:25 +0000

    blktests/oscheck.sh: move processing osfiles to a helper
    This will allow us to use them later in another script.
    
    Signed-off-by: Luis Chamberlain <mcgrof@kernel.org>

--- workflows/blktests/scripts/oscheck.sh
+++ workflows/blktests/scripts/oscheck.sh
@@ -3,30 +3,13 @@
 
 # OS wrapper for blktests check.sh
 
-OSCHECK_OSFILE_PREFIX=""
-OSCHECK_SUBSYSTEM="blktests"
-
 DRY_RUN="false"
-EXPUNGE_FLAGS=""
 ONLY_SHOW_CMD="false"
 VERBOSE="false"
 ONLY_TEST_GROUP=""
 ONLY_CHECK_DEPS="false"
-ONLY_QUESTION_DISTRO_KERNEL="false"
 PRINT_START="false"
 PRINT_DONE="false"
-
-if [ ! -z "$OSCHECK_SUBSYSTEM" ]; then
-	OSCHECK_OSFILE_PREFIX="_blktests"
-fi
-
-if [ -z "$OSCHECK_ONLY_RUN_DISTRO_KERNEL" ]; then
-	OSCHECK_ONLY_RUN_DISTRO_KERNEL="false"
-fi
-
-if [ -z "$OSCHECK_CUSTOM_KERNEL" ]; then
-	OSCHECK_CUSTOM_KERNEL="false"
-fi
 
 # Where we stuff the arguments we will pass to ./check
 declare -a CHECK_ARGS
@@ -122,155 +105,6 @@ parse_args()
 
 parse_args $@
 
-oscheck_add_expunge_no_dups()
-{
-	echo $EXPUNGE_TESTS | grep -q "$1" 2>&1 > /dev/null
-	if [[ $? -eq 0 ]]; then
-		return 3
-	fi
-
-	EXPUNGE_FLAGS="$EXPUNGE_FLAGS -x ${1}"
-	EXPUNGE_TESTS="$EXPUNGE_TESTS ${1}"
-	let EXPUNGE_TESTS_COUNT=$EXPUNGE_TESTS_COUNT+1
-	return 0
-}
-
-
-oscheck_add_expunge_if_exists_no_dups()
-{
-	if [[ ! -e tests/$1 ]]; then
-		return 1
-	fi
-	oscheck_add_expunge_no_dups $1
-	return $?
-}
-
-os_has_special_expunges()
-{
-	if [ ! -e $OS_FILE ]; then
-		return 1
-	fi
-	declare -f ${OSCHECK_ID}${OSCHECK_OSFILE_PREFIX}_special_expunges > /dev/null;
-	return $?
-}
-
-oscheck_handle_special_expunges()
-{
-	os_has_special_expunges $OSCHECK_ID
-	if [ $? -eq 0 ] ; then
-		${OSCHECK_ID}${OSCHECK_OSFILE_PREFIX}_special_expunges
-	fi
-}
-
-oscheck_get_group_files()
-{
-	if [ "$EXPUNGE_LIST" = "true" ]; then
-		echo "Looking for files which we should expunge in directory $BLOCK_EXCLUDE_DIR ..."
-	fi
-	BAD_FILES=""
-	if [[ -d $BLOCK_EXCLUDE_DIR ]]; then
-		BAD_FILES=$(find $BLOCK_EXCLUDE_DIR -type f \( -iname \*.bad -o -iname \*.dmesg \) | sed -e 's|'$BLOCK_EXCLUDE_DIR'||')
-	fi
-	for i in $BAD_FILES; do
-		COLS=$(echo $i | awk -F"/" '{print NF}')
-		if [[ $COLS -ne 3 ]]; then
-			continue
-		fi
-
-		BDEV=$(echo $i | awk -F"/" '{print $1}')
-		GROUP=$(echo $i | awk -F"/" '{print $2}')
-		BADFILE=$(echo $i | awk -F"/" '{print $3}')
-
-		if [[ "$RUN_GROUP" != "" ]]; then
-			if [[ "$GROUP" != "$RUN_GROUP" ]]; then
-				continue
-			fi
-		fi
-		COLS=$(echo $BADFILE | awk -F"." '{print NF}')
-		if [[ $COLS -lt 2 ]]; then
-			continue
-		fi
-		NUMBER=$(echo $BADFILE | awk -F"." '{print $1}')
-		ENTRY="$GROUP/$NUMBER"
-		oscheck_add_expunge_if_exists_no_dups $ENTRY
-		if [[ $? -eq 1 && "$EXPUNGE_LIST" = "true" ]]; then
-			echo "$ENTRY is not a test present on your version of blktests"
-		fi
-	done
-	OSCHECK_EXPUNGE_FILE="$(dirname $(readlink -f $0))/../expunges/$(uname -r)/failures.txt"
-	if [[ "$EXPUNGE_LIST" = "true" ]]; then
-		echo "Looking to see if $OSCHECK_EXPUNGE_FILE exists and has any entries we may have missed ..."
-	fi
-	if [[ -f $OSCHECK_EXPUNGE_FILE ]]; then
-		if [[ "$EXPUNGE_LIST" = "true" ]]; then
-			echo "$OSCHECK_EXPUNGE_FILE exists, processing its expunges ..."
-		fi
-		BAD_EXPUNGES=$(cat $OSCHECK_EXPUNGE_FILE| awk '{print $1}')
-		for i in $BAD_EXPUNGES; do
-			oscheck_add_expunge_no_dups $i
-		done
-	fi
-	if [ -e $OS_FILE ]; then
-		OSCHECK_EXPUNGE_FILE="$(dirname $(readlink -f $0))/../expunges/${OSCHECK_ID}/${VERSION_ID}/failures.txt"
-		if [[ "$EXPUNGE_LIST" = "true" ]]; then
-			echo "Looking to see if $OSCHECK_EXPUNGE_FILE exists and has any entries we may have missed ..."
-		fi
-		if [[ -f $OSCHECK_EXPUNGE_FILE ]]; then
-			if [[ "$EXPUNGE_LIST" = "true" ]]; then
-				echo "$OSCHECK_EXPUNGE_FILE exists, processing its expunges ..."
-			fi
-			BAD_EXPUNGES=$(cat $OSCHECK_EXPUNGE_FILE| awk '{print $1}')
-			for i in $BAD_EXPUNGES; do
-				oscheck_add_expunge_no_dups $i
-			done
-		fi
-	fi
-}
-
-oscheck_count_check()
-{
-	if [[ "$EXPUNGE_TESTS_COUNT" -eq 0 ]]; then
-		if [[ "$RUN_GROUP" != "" ]]; then
-			echo "No expunges for blktests on test group $GROUP -- a perfect kernel!"
-		else
-			echo "No expunges for blktests -- a perfect kernel!"
-		fi
-	fi
-}
-
-oscheck_handle_group_expunges()
-{
-	EXPUNGE_FLAGS=""
-	if [ "$EXPUNGE_LIST" = "true" ]; then
-		if [[ "$RUN_GROUP" != "" ]]; then
-			echo "List of blktests expunges for group $GROUP:"
-		else
-			echo "List of blktests expunges:"
-		fi
-	fi
-
-	oscheck_handle_special_expunges
-
-	if [ -e $OS_FILE ]; then
-		BLOCK_EXCLUDE_DIR="${OSCHECK_EXCLUDE_PREFIX}/${OSCHECK_ID}/${VERSION_ID}/"
-		OSCHECK_EXCLUDE_DIR="$BLOCK_EXCLUDE_DIR"
-		if [ "$OSCHECK_CUSTOM_KERNEL" == "true" ]; then
-			if [ "$OSCHECK_ONLY_RUN_DISTRO_KERNEL" != "true" ]; then
-				BLOCK_EXCLUDE_DIR="${OSCHECK_EXCLUDE_PREFIX}/$(uname -r)/"
-				OSCHECK_EXCLUDE_DIR="$BLOCK_EXCLUDE_DIR"
-			fi
-		fi
-		oscheck_get_group_files
-	fi
-
-	# One more check for distro agnostic expunges. Right now this is just an
-	# informal group listing. Only files named after a section would be treated
-	# as generic expunges for blktests, but as of right now we have none.
-	BLOCK_EXCLUDE_DIR="${OSCHECK_EXCLUDE_PREFIX}/any/"
-	# Don't update OSCHECK_EXCLUDE_DIR as these are extra files only.
-	oscheck_get_group_files
-}
-
 oscheck_run_cmd()
 {
 	if [ "$ONLY_SHOW_CMD" = "false" ]; then
@@ -288,10 +122,6 @@ oscheck_run_groups()
 		echo "run blktests blktestsstart/000 at $NOW" > /dev/kmsg
 	fi
 
-	if [[ "$LIMIT_TESTS" == "" ]]; then
-		oscheck_handle_group_expunges
-		oscheck_count_check
-	fi
 	OSCHECK_CMD="./check ${RUN_GROUP} $EXPUNGE_FLAGS ${CHECK_ARGS[@]}"
 	oscheck_run_cmd
 
@@ -303,44 +133,6 @@ oscheck_run_groups()
 
 _cleanup() {
 	echo "Done"
-}
-
-os_has_distro_kernel_check_handle()
-{
-	if [ ! -e $OS_FILE ]; then
-		return 1
-	fi
-	declare -f ${OSCHECK_ID}_distro_kernel_check > /dev/null;
-	return $?
-}
-
-oscheck_distro_kernel_check()
-{
-	os_has_distro_kernel_check_handle
-	if [ $? -eq 0 ] ; then
-		${OSCHECK_ID}_distro_kernel_check
-		if [ $? -ne 0 ] ; then
-			OSCHECK_CUSTOM_KERNEL="true"
-			if [ "$ONLY_QUESTION_DISTRO_KERNEL" = "true" ]; then
-				echo "You are not running a distribution packaged kernel:"
-				uname -a
-				exit 1
-			fi
-			if [ "$OSCHECK_ONLY_RUN_DISTRO_KERNEL" == "true" ]; then
-				echo "Not running a distro kernel, skipping..."
-				echo "If you want to run this test disable:"
-				echo "OSCHECK_ONLY_RUN_DISTRO_KERNEL"
-				exit 1
-			fi
-			echo "Running custom kernel: $(uname -a)"
-		else
-			if [ "$ONLY_QUESTION_DISTRO_KERNEL" = "true" ]; then
-				echo "Running distro kernel"
-				uname -a
-				exit 0
-			fi
-		fi
-	fi
 }
 
 check_check()
@@ -365,4 +157,5 @@ fi
 tmp=/tmp/$$
 trap "_cleanup; exit \$status" 0 1 2 3 15
 
+oscheck_lib_set_expunges
 oscheck_run_groups
