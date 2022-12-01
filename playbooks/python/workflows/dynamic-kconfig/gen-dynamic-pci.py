c5e20dbcde7142b4e2051ef062585c62986e61d6
Luis Chamberlain <mcgrof@kernel.org>
2022-11-30 16:38:52 -0800

    gen-dynamic-pci.py: enhance nvme kconfig names
    When using PCI-E passthrough, seeing a device name by PCI device
    sdevice name doesn't really make it easy for us to determine if
    its OK to use that devices or if its the desired target device.
    A more sane approach is to use the /dev/nvme* name and also the
    resepective NVMe model / firmware revision. So add special handling
    for NVMe drives so that you can easily identify what device
    you'd be using for PCI-E passthrough.
    
    Anwway, I figure I should mention that PCI-E passthrough works
    as expected and so this goes tested now with this working
    successfully. For example, here is the lspci output after
    using the new 'make dyconfig' to select PCI-passthrough for
    two NVMe Samsung drives onto the guest called testing64:
    
    vagrant@testing64 ~ $ sudo lspci | grep -i nvm
    05:00.0 Non-Volatile memory controller: Samsung Electronics Co Ltd NVMe SSD Controller SM961/PM961/SM963
    06:00.0 Non-Volatile memory controller: Samsung Electronics Co Ltd NVMe SSD Controller PM173Xa
    21:00.0 Non-Volatile memory controller: Red Hat, Inc. QEMU NVM Express Controller (rev 02)
    22:00.0 Non-Volatile memory controller: Red Hat, Inc. QEMU NVM Express Controller (rev 02)
    23:00.0 Non-Volatile memory controller: Red Hat, Inc. QEMU NVM Express Controller (rev 02)
    24:00.0 Non-Volatile memory controller: Red Hat, Inc. QEMU NVM Express Controller (rev 02)
    
    Signed-off-by: Luis Chamberlain <mcgrof@kernel.org>

--- playbooks/python/workflows/dynamic-kconfig/gen-dynamic-pci.py
+++ playbooks/python/workflows/dynamic-kconfig/gen-dynamic-pci.py
@@ -11,8 +11,57 @@ import re
 
 dynpci_kconfig_ansible_python_dir = os.path.dirname(os.path.abspath(__file__))
 passthrough_prefix = "KDEVOPS_DYNAMIC_PCIE_PASSTHROUGH"
+sys_bus_prefix = "/sys/bus/pci/devices/"
 
 debug = 0
+
+def get_first_dir(path):
+    if len(os.listdir(path)) > 0:
+        return os.listdir(path)[0]
+    return None
+
+def get_sysname(sys_path, entry):
+    sys_entry_path = sys_path + entry
+    if not os.path.isfile(sys_entry_path):
+        return None
+    entry_fd = open(sys_entry_path, 'r')
+    line = entry_fd.readlines()[0]
+    line = line.strip()
+    entry_fd.close()
+    return line
+
+# kconfig does not like some characters
+def strip_kconfig_name(name):
+    fixed_name = name.replace("\"", "")
+    fixed_name = fixed_name.replace("'", "")
+    return fixed_name
+
+def get_special_device_nvme(pci_id):
+    pci_id_name = strip_kconfig_name(pci_id)
+    sys_path = sys_bus_prefix + pci_id + "/nvme/"
+    if not os.path.isdir(sys_path):
+        return None
+    block_device_name = get_first_dir(sys_path)
+    if not block_device_name:
+        return None
+    block_sys_path = sys_path + block_device_name + "/"
+    model = get_sysname(block_sys_path, "model")
+    if not model:
+        return None
+    fw = get_sysname(block_sys_path, "firmware_rev")
+    if not fw:
+        return None
+    return pci_id_name + " - /dev/" + block_device_name + " - " + model + " with FW %s" % fw
+
+
+def get_kconfig_device_name(pci_id, sdevice):
+    default_name = pci_id + " - " + sdevice
+    special_name = None
+    if os.path.isdir(sys_bus_prefix + pci_id + "/nvme"):
+        special_name = get_special_device_nvme(pci_id)
+    if not special_name:
+        return strip_kconfig_name(default_name)
+    return strip_kconfig_name(special_name)
 
 def add_pcie_kconfig_string(prefix, val, name):
     config_name = prefix + "_" + name.upper()
@@ -30,21 +79,22 @@ def add_pcie_kconfig_name(config_name, sdevice):
     sys.stdout.write("\t  target guest.\n")
     sys.stdout.write("\n")
 
-def add_pcie_kconfig_entry(sdevice, domain, bus, slot, function, config_id):
+def add_pcie_kconfig_entry(pci_id, sdevice, domain, bus, slot, function, config_id):
     prefix = passthrough_prefix + "_%04d" % config_id
-    add_pcie_kconfig_name(prefix, sdevice)
+    name = get_kconfig_device_name(pci_id, sdevice.strip())
+    add_pcie_kconfig_name(prefix, name)
     add_pcie_kconfig_string(prefix, domain, "domain")
     add_pcie_kconfig_string(prefix, bus, "bus")
     add_pcie_kconfig_string(prefix, slot, "slot")
     add_pcie_kconfig_string(prefix, function, "function")
 
 def add_new_device(slot, sdevice, possible_id):
-    line = slot.strip()
+    slot = slot.strip()
     # Example expeced format 0000:2d:00.0
     m = re.match(r"^(?P<DOMAIN>\w+):"
                   "(?P<BUS>\w+):"
                   "(?P<MSLOT>\w+)\."
-                  "(?P<FUNCTION>\w+)$", line)
+                  "(?P<FUNCTION>\w+)$", slot)
     if not m:
         return possible_id
 
@@ -59,6 +109,7 @@ def add_new_device(slot, sdevice, possible_id):
     sdevice = sdevice.strip()
 
     if debug:
+        sys.stdout.write("\tslot: %s\n" % (slot))
         sys.stdout.write("\tdomain: %s\n" % (domain))
         sys.stdout.write("\tbus: %s\n" % (bus))
         sys.stdout.write("\tslot: %s\n" % (mslot))
@@ -67,7 +118,7 @@ def add_new_device(slot, sdevice, possible_id):
     if possible_id == 1:
         sys.stdout.write("# Automatically generated PCI-E passthrough Kconfig by kdevops\n\n")
 
-    add_pcie_kconfig_entry(sdevice, domain, bus, mslot, function, possible_id)
+    add_pcie_kconfig_entry(slot, sdevice, domain, bus, mslot, function, possible_id)
 
     return possible_id
 
