14c461673203312923678ae718468ab4a27806f3
Luis Chamberlain <mcgrof@kernel.org>
2022-12-19 17:30:14 -0800

    vagrant/libvirt: add smart storage pool heuristic
    A power usage of kdevops means git cloning it on many directories,
    one per goal. So for instance I have:
    
    /dev/nvme0n1 with xfs   on /data1/
    /dev/nvme1n1 with btrfs on /data2/
    /dev/nvme2n1 with ext4  on /data3/
    
    And then I test xfs on /data1/ on linux-next so I have:
    
    /data1/next/xfs/kdevops/
    
    But likewise I also test XFS using btrfs on the host, so I have also:
    
    /data2/next/xfs/kdevops/
    
    And likewise I test xfs on ext4 on the host too... so I have:
    
    /data3/next/xfs/kdevops/
    
    But I also test CXL so I have:
    
    /data1/cxl/kdevops/
    
    And so on. To make most efficient use of the nvme drives I want my
    guest images to be local to the same nvme drive so I create a data1,
    data2 and data3 storage pool, and I allow kdevops to create these
    storage pool paths for me. Once created though I want kdevops to infer
    the pool name and path for me as I want to be lazy and I want sensible
    defaults.
    
    And so, this adds precisely that heuristic of looking for existing
    pools and if they exist looking to see if the base directory I am
    in has a directory which a storage pool mentions. If so then it uses
    that storage pool name and path.
    
    I only enable this for DISTRO_DEBIAN, and while this should work for
    other distros, folks can easily modify their own distro default once
    they verify it works there too. I add these guards to ensure I don't
    break any other distro workflow by default.
    
    All distros should eventually migrate to this new heuristic.
    
    Signed-off-by: Luis Chamberlain <mcgrof@kernel.org>

---
+++ scripts/get_libvirsh_pool_name.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Tries to infer your preferred libvirt storage pool name.
+# If you are using kdevops in a directory like say /data1/next/kdevops
+# this will look to see if /data1/ is the path of a storage pool name
+# and if so return that name.
+
+SCRIPTS_DIR=$(dirname $0)
+source ${SCRIPTS_DIR}/libvirt_pool.sh
+
+BASE_DIR=$(echo ${PWD} | awk -F"/" '{print $2}')
+OS_FILE="/etc/os-release"
+USES_QEMU_USER_SESSION="n"
+CAN_SUDO="n"
+REQ_SUDO=""
+POOL_LIST=""
+
+get_pool_vars
+VIRSH_WORKS=$(virsh_works)
+if [[ "$VIRSH_WORKS" == "n" ]]; then
+	echo "default"
+fi
+
+virsh_get_pool_list
+
+DOES_POOL_EXIST=$(virsh_path_in_pool_list_exists)
+if [[ "$DOES_POOL_EXIST" != "y" ]]; then
+	echo "default"
+fi
+
+virsh_path_pool_list_name
---
+++ scripts/libvirt_pool.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+# Helpers to work with virsh pools
+
+get_pool_vars()
+{
+	if [[ -f $OS_FILE ]]; then
+		grep -qi fedora $OS_FILE
+		if [[ $? -eq 0 ]]; then
+			USES_QEMU_USER_SESSION="y"
+		fi
+	fi
+
+	SUDO_ASKPASS=/bin/false sudo -A whoami 2>&1 |
+	if [[ $? -ne 0 ]]; then
+		CAN_SUDO="y"
+	fi
+
+	if [[ "$USES_QEMU_USER_SESSION" != "y" ]]; then
+		REQ_SUDO="sudo"
+	fi
+}
+
+virsh_works()
+{
+	$REQ_SUDO which virsh 2>&1 > /dev/null
+	if [[ $? -ne 0 ]]; then
+		echo n
+		return
+	fi
+	$REQ_SUDO virsh pool-list 2>&1 > /dev/null
+	if [[ $? -eq 0 ]]; then
+		echo y
+	else
+		echo n
+	fi
+}
+
+
+virsh_get_pool_list()
+{
+	POOL_LIST=$($REQ_SUDO virsh pool-list| egrep -v "Name|\-\-\-\-\-"|  sed -e '/^$/d' | awk '{print $1}')
+}
+
+virsh_path_in_pool_list_exists()
+{
+	for i in $POOL_LIST; do
+		POOL_PATH=$($REQ_SUDO virsh pool-dumpxml $i | grep path | sed -e 's|<path>||'g | sed -e 's|</path>||'g | awk '{print $1}')
+		echo $POOL_PATH | grep -q $BASE_DIR
+		if [[ $? -eq 0 ]]; then
+			echo y
+			exit
+		fi
+	done
+	echo n
+}
+
+virsh_path_pool_list_name()
+{
+	for i in $POOL_LIST; do
+		POOL_PATH=$($REQ_SUDO virsh pool-dumpxml $i | grep path | sed -e 's|<path>||'g | sed -e 's|</path>||'g | awk '{print $1}')
+		echo $POOL_PATH | grep -q $BASE_DIR
+		if [[ $? -eq 0 ]]; then
+			echo $i
+			exit
+		fi
+	done
+}
+
+virsh_path_pool_list_path()
+{
+	for i in $POOL_LIST; do
+		POOL_PATH=$($REQ_SUDO virsh pool-dumpxml $i | grep path | sed -e 's|<path>||'g | sed -e 's|</path>||'g | awk '{print $1}')
+		echo $POOL_PATH | grep -q $BASE_DIR
+		if [[ $? -eq 0 ]]; then
+			echo $POOL_PATH
+			exit
+		fi
+	done
+}
