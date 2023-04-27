476b19637002d536cd0577aad81ef7152126e826
Jeff Layton <jlayton@kernel.org>
2023-04-27 16:21:51 -0400

    workflows: use "grep -E" instead of egrep in shell scripts
    I'm getting warnings about egrep being deprecated and about extraneous
    '\' chars in the regex. Switch to grep -E and drop the unneeded escapes.
    
    Signed-off-by: Jeff Layton <jlayton@kernel.org>

--- scripts/workflows/fstests/remove-common-failures.sh
+++ scripts/workflows/fstests/remove-common-failures.sh
@@ -41,7 +41,7 @@ done
 
 TMP_FILE=$(mktemp)
 for i in $FILES; do
-	cat $i | egrep -v "${COMMON_EXPUNGES[@]}" | sort | uniq > $TMP_FILE
+	cat $i | grep -E -v "${COMMON_EXPUNGES[@]}" | sort | uniq > $TMP_FILE
 	mv $TMP_FILE $i
 	SIZE=$(du -b $i | awk '{print $1}')
 	if [[ -d .git ]]; then
