00fb095530ea2dfeddb719acf26029927b3cb4c6
Jeff Layton <jlayton@kernel.org>
2023-05-25 14:03:37 -0400

    pynfs: use script to run pynfs jobs in parallel
    Just background both jobs using shell job control and then try to
    foreground both to wait until they complete. For now, this is more
    robust than trying to rely on ansible's async task support.
    
    Signed-off-by: Jeff Layton <jlayton@kernel.org>

---
+++ workflows/pynfs/scripts/run_pynfs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Enable job control
+set -m
+
+pynfs_versions="4.0 4.1"
+
+# kick off jobs to run in the background in parallel
+for version in $pynfs_versions; do
+	cd ${PYNFS_DATA}/nfs${version}
+	./testserver.py --json=${PYNFS_DATA}/pynfs-${version}-results.json --maketree --uid=0 --gid=0 localhost:${NFSD_EXPORT_PATH}/pynfs/${version} all &
+done
+
+# wait for each to complete
+for version in $pynfs_versions; do
+	fg || true
+done
