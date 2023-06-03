adf9a9dd6c759a061bff585d1ce3a7ae12eed9f3
Jeff Layton <jlayton@kernel.org>
2023-06-03 07:39:56 -0400

    pynfs: run the test from baseline host
    Instead of playing shenanigans with the groups and trying to find a
    private IP address, just utilize the entries in /etc/hosts to run pynfs.
    
    Signed-off-by: Jeff Layton <jlayton@kernel.org>

--- workflows/pynfs/scripts/run_pynfs.sh
+++ workflows/pynfs/scripts/run_pynfs.sh
@@ -8,7 +8,7 @@ pynfs_versions="4.0 4.1"
 # kick off jobs to run in the background in parallel
 for version in $pynfs_versions; do
 	cd ${PYNFS_DATA}/nfs${version}
-	./testserver.py --json=${PYNFS_DATA}/pynfs-${version}-results.json --maketree --uid=0 --gid=0 localhost:${NFSD_EXPORT_PATH}/pynfs/${version} all &
+	./testserver.py --json=${PYNFS_DATA}/pynfs-${version}-results.json --maketree --uid=0 --gid=0 ${NFSD_EXPORT}/pynfs/${version} all &
 done
 
 # wait for each to complete
