33cf2042ad58ab48261a28875429d4fc6c0989e3
Chandan Babu R <chandan.babu@oracle.com>
2023-06-07 10:35:29 +0530

    oci: Update ssh configuration with appropriate IP address
    Update ssh configuration with either private or public IP address based on
    whether the user requested for private or public IP address for the instance.
    
    Signed-off-by: Chandan Babu R <chandan.babu@oracle.com>

--- terraform/oci/update_ssh_config_use.tf
+++ terraform/oci/update_ssh_config_use.tf
@@ -1,4 +1,8 @@
 locals {
   shorthosts	= oci_core_instance.kdevops_instance.*.display_name
-  ipv4s		= oci_core_instance.kdevops_instance.*.private_ip
+  ipv4s		= (
+    var.oci_assign_public_ip == "false" ?
+    oci_core_instance.kdevops_instance.*.private_ip :
+    oci_core_instance.kdevops_instance.*.public_ip
+  )
 }
