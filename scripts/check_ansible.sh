e08a03c93e0ae7014017ec526a018701e7049bc4
DongDongJu <commisori28@gmail.com>
2023-08-03 20:42:24 +0000

    scripts/check_ansible.sh: Add ansible installation script
    Add new script for checking whether ansible-playbook installed or not.
    If its not installed in the system then try to install by pip.
    
    Signed-off-by: Dongjoo Seo <dongjoo1.seo@samsung.com>

---
+++ scripts/check_ansible.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if which ansible-playbook >/dev/null; then
+    echo "ansible existed"
+else
+    if which pip > /dev/null; then
+    	echo "Start to install ansible"
+		pip install ansible
+    	echo "Ansible installed"
+	else
+		echo "install pip befoer starting this script"
+	fi
+fi
